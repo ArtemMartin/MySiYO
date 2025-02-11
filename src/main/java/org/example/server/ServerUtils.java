6b95a8f6b6b04e83fcb6ea4a90f7e777942e55de
Artem Stypak <arcenivashenko@yandex.ru>
2025-02-11 18:38:07 +0300

    jar for last comm

---
+++ src/main/java/org/example/server/ServerUtils.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example.server;
+
+/**
+ *
+ * @author artem
+ */
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.logging.Logger;
+
+public class ServerUtils {
+
+    public static String getServerIPv4Address() {
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = inetAddresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof java.net.Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Logger.getLogger(ServerUtils.class.getName()).info(e.getMessage());
+        }
+        return "Не удалось получить IPv4-адрес";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("IPv4-адрес сервера: " + getServerIPv4Address());
+    }
+}
